5b744dbaed09bced1820820e6dfc116ebf932e6f
Todd Skelton <todd.skelton@outlook.com>
2026-06-29 17:21:22 -0500

    Remove marketplace proof access gate (#3062)

--- deployables/marketplace/app/routes/chrome-devtools.ts
+++ deployables/marketplace/app/routes/chrome-devtools.ts
@@ -1,9 +1,6 @@
 import type { LoaderFunctionArgs } from "react-router";
-import { requireMarketplaceProofAccess } from "../proof-access.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await requireMarketplaceProofAccess(request);
-
   return new Response(null, {
     status: 204,
     headers: {
--- deployables/marketplace/app/routes/robots.ts
+++ deployables/marketplace/app/routes/robots.ts
@@ -1,10 +1,7 @@
 import type { LoaderFunctionArgs } from "react-router";
-import { requireMarketplaceProofAccess } from "../proof-access.server";
 import { buildMarketplaceRobotsTxt, shouldIndexMarketplace } from "../seo";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await requireMarketplaceProofAccess(request);
-
   const origin = new URL(request.url).origin;
   const body = buildMarketplaceRobotsTxt({ origin, shouldIndex: shouldIndexMarketplace() });
 
