7fe524efd087f005457226a95aadd0a62c3bfbc7
Todd Skelton <todd.skelton@outlook.com>
2026-07-06 01:06:32 -0500

    Fix DOKS worker email provider default (#4436)

--- bounded-contexts/commercial-terms/support/runtime-support/schema.ts
+++ bounded-contexts/commercial-terms/support/runtime-support/schema.ts
@@ -1,7 +1,11 @@
+import { eventCorePostgresSchemaSql } from "@chase-sets/event-core-postgres";
 import { agreementSchemaSql } from "../../features/agreements/read-model/schema";
 import { resolutionAccountSchemaSql } from "../../features/resolutions/integrations/account-source/account-schema";
 import { scheduleSchemaSql } from "../../features/schedules/read-model/schema";
 
-export const commercialTermsSchemaSql = [resolutionAccountSchemaSql, scheduleSchemaSql, agreementSchemaSql].join(
-  "\n\n",
-);
+export const commercialTermsSchemaSql = [
+  eventCorePostgresSchemaSql,
+  resolutionAccountSchemaSql,
+  scheduleSchemaSql,
+  agreementSchemaSql,
+].join("\n\n");
