e032c95e7a5d43df3f10d8bd702b20044bb419f1
Todd Skelton <todd.skelton@outlook.com>
2026-09-11 03:39:52 -0500

    chore(review): align route adapter inventories

--- bounded-contexts/channels/routes/marketplace/account-channels-connection.tsx
+++ bounded-contexts/channels/routes/marketplace/account-channels-connection.tsx
@@ -1,8 +1,11 @@
+import { loader as connectionLoader } from "../../features/connections/ui/account-channels-connection-route-adapter";
+
 export {
   action,
   default,
   downloadAction,
-  loader,
   meta,
   readActionError,
 } from "../../features/connections/ui/account-channels-connection-route-adapter";
+
+export const loader = connectionLoader;
